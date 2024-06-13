0ed2b35dcc8ef27a170292c57d3ac821149fd25c
unknown <qureshinasra47@gmail.com>
2024-06-13 17:48:55 +0530

    msg

---
+++ script.js
@@ -0,0 +1,157 @@
+//console.log("hello nasra1234");
+//console.log("Hello Techno students")
+
+//variables
+//let message = "The Password Is Incorrect";
+//define the variable and assign value to it 
+// console.log(message);
+// alert(message);
+
+//define variable hello and assign value as aquib
+// let hello = "iram";
+//  console.log(hello);
+
+//  var newdata = "haris";
+//  console.log(newdata);
+
+// let messagebox;
+// messagebox = "world";
+// console.log(messagebox);
+
+
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+// let admin;
+// let name;
+// name = "anam";
+// admin = name;
+// alert(admin);
+
+ //constants 
+ //const birthday = "08.12.2001";
+//  birthday = "10.11.2001";
+//  console.log(birthday);
+
+// refer URL 
+// https://javascript.info/variables
+
+// Data types in js 
+// 1.number 
+// let n = 123;
+//let nf = 12.344;
+//console.log(n)
+
+
+// 2.bigint
+// let bigint = 1234567890n;
+// alert( bigint )
+// console.log(bigint)
+
+// 3.string
+// let str = "Hello";
+// let str2 = 'Single quotes are ok too';
+// let phrase = ${str} can embed another ${str2};
+//  console.log(phrase);
+//  alert( the result is ${1 + 2} );
+
+//4.boolean
+// let isGreater = 4 < 7;
+// alert( isGreater );
+
+// 5.null 
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”. 
+//  let age = null;
+//  console.log(age);
+
+// 6.undefine 
+//  let data;123
+// console.log(data);
+
+// 7.object for more complex data structures.
+// 8.symbol for unique identifiers.
+
+// url 
+// https://javascript.info/types
+
+
+// Basic operators 
+
+// addition 
+//  let apples = "4";
+//  let oranges = "6";
+//  alert( apples + oranges );
+
+// 1.arithmatic operator 
+//  a1 = 20;
+//  a2 = 30;
+//  a3 = a1 + a2;
+//  console.log(a3);
+
+// 2.assignment operator 
+//  a1 = 15;  a1 = 30 + 15
+//  a1 += 15;
+//  console.log(a1);
+
+//  let n = 2;
+//  n += 5;  
+// now n = 7 (same as n = n + 5)
+//   n *= 2;
+//  now n = 14 (same as n = n * 2)
+
+//   alert( n ); // 14
+
+// let x = 1;
+// x = -x;
+// alert( x ); //  unary negation was applied
+
+// let z = 1, y = 3;
+// alert( y - z ); 
+
+// division  8 / 3
+// alert( 8 % 3 ); 
+
+// exponentional 2² 
+// alert( 2 ** 2 ); // 2² = 4
+// alert( 2 ** 3 ); // 2³ = 8
+// alert( 2 ** 4 ); // 2⁴ = 16
+
+// '1' is string ...therefore value is 12
+//  alert( '1' + 2 ); // "12"
+//  alert( 2 + '1' ); // "21"
+
+//  alert(2 + 2 + '1' ); 
+
+// 3.string operator 
+  let s = "my" + "string";
+  alert(s); // mystring
+
+// 4.increment and decement operator 
+//  ++x pre-increment ...increment x by 1 then return x 
+// x++ post-increment ...return x than increment by 1
+// --x pre-decrement ...decrement x by 1 then return x
+// x-- post-decrement ...return x than decrement by 1
+
+//  let f;
+//  f = 10;
+//  console.log(++f);  //11
+//  console.log(f);     //11
+
+//  let g;
+//  g = 12;
+//  console.log(g++);  //12
+//  console.log(g);     //13
+
+// let h;
+// h = 10;
+// console.log(--h);  //9
+// console.log(h);     //9
+
+let i;
+i = 12;
+console.log(i--); //12
+console.log(i);  //11
+
+
+
